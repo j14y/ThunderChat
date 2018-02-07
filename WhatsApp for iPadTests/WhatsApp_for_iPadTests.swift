3f1afb583b04ab7e44ae9cc60c2d45cbda91869e
jordychetty <jxrrdy@gmail.com>
2018-02-07 23:56:09 +0200

    Initial Commit

---
+++ WhatsApp for iPadTests/WhatsApp_for_iPadTests.swift
@@ -0,0 +1,36 @@
+//
+//  WhatsApp_for_iPadTests.swift
+//  WhatsApp for iPadTests
+//
+//  Created by Jordy Chetty on 2018/02/07.
+//  Copyright © 2018 Intrinzic Studios. All rights reserved.
+//
+
+import XCTest
+@testable import WhatsApp_for_iPad
+
+class WhatsApp_for_iPadTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
