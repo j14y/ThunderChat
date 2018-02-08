1abf65da561685fc5b2728bc6cf718c210505f2e
jordychetty <jxrrdy@gmail.com>
2018-02-08 06:11:38 +0200

    Beta release
    - Still need to fix issues with video playback in web view but perfectly functional aside from that

--- WhatsApp for iPad/Supporting Files/AppDelegate.swift
+++ WhatsApp for iPad/Supporting Files/AppDelegate.swift
@@ -74,7 +74,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }()
 
     // MARK: - Core Data Saving support
-
     func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
--- WhatsApp for iPad/ViewController.swift
+++ WhatsApp for iPad/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        webView.scrollView.bounces = false
+        webView.allowsInlineMediaPlayback = true
+        webView.scrollView.isScrollEnabled = false
+        let link = URL(string: "https://web.whatsapp.com")
+        let request = URLRequest (url: link!, cachePolicy: URLRequest.CachePolicy.reloadIgnoringCacheData, timeoutInterval: 60)
+        UserDefaults.standard.register(defaults: ["UserAgent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36"])
+        self.webView.loadRequest(request)
     }
 
     override func didReceiveMemoryWarning() {
